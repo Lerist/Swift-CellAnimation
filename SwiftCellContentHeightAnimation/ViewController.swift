31f78e7e5f728e7067428b23506d02ead4b90aaf
Marvin <t3zhan@uwaterloo.ca>
2017-01-25 21:54:58 +0800

    Initial Commit

---
+++ SwiftCellContentHeightAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCellContentHeightAnimation
+//
+//  Created by Monster on 2017-01-25.
+//  Copyright © 2017 Monster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
